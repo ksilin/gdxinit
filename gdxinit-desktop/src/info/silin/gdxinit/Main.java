fa4f161bae9c2664b19d6a256d83f23cc3cc7414
kostja <konstantin@silin.info>
2013-05-01 11:35:56 +0200

    added gcx-tools, packing textures on desktop start

--- gdxinit-desktop/src/info/silin/gdxinit/Main.java
+++ gdxinit-desktop/src/info/silin/gdxinit/Main.java
@@ -2,9 +2,18 @@ package info.silin.gdxinit;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.tools.imagepacker.TexturePacker2;
+import com.badlogic.gdx.tools.imagepacker.TexturePacker2.Settings;
 
 public class Main {
 	public static void main(String[] args) {
+
+		Settings settings = new Settings();
+		settings.maxWidth = 512;
+		settings.maxHeight = 512;
+		TexturePacker2.process(settings, "../gdxinit-android/assets",
+				"../gdxinit-android/assets", "game");
+
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
 		cfg.title = "gdxinit";
 		cfg.useGL20 = true;
