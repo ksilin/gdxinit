02bf2714215c2ea8a548735b285d1c9dd9ac3c6c
kostja <konstantin@silin.info>
2013-07-30 14:02:41 +0200

    added state to Projectile

--- gdxinit/src/info/silin/gdxinit/entity/Projectile.java
+++ gdxinit/src/info/silin/gdxinit/entity/Projectile.java
@@ -6,11 +6,19 @@ public class Projectile extends Entity {
 
 	public static final float SIZE = 0.11f;
 
+	public enum State {
+		IDLE, FLYING, EXPLODING
+	}
+
+	public State state;
+
 	public Projectile(Vector2 position, Vector2 velocity) {
 		this.position = position;
 		this.velocity = velocity;
 		this.bounds.width = SIZE;
 		this.bounds.height = SIZE;
 		this.size = SIZE;
+		this.state = State.FLYING;
 	}
+
 }
